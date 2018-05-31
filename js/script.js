9b9aa929d32cb5f70fccb8eaa0fe9b0b3af4b7e6
Bakyt <zhbakyt@mail.ru>
2018-05-31 22:29:21 +0300

    Это наш первый комит

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var money = +prompt('Какой ваш бюджет на ммесяц?', '50 000');
+var name = prompt ('Название вашего магазина');
+
+mainList = {
+	name: "John",
+	age: 25,
+	isMarried: false
+}
+
+var myNumber = 'Привет я стока javaScript';
+var myNumber2 = 5.5;
+alert(typeof myNumber);
+alert(typeof myNumber2);
